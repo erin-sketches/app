feb4dec375e671cc5be1f5e998213a913add3f92
erin <erin.sketches@gmail.com>
2020-11-21 16:21:16 -0500

    Use npm alias trick to get preact-compat renamed so we can use preact and plotly together. Figure out darktheme, yay.

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Lessons learned
+
+* Instead of `npm -g` use `npx preact-cli create simple` to create a project (npx allows you to run npm-hosted binaries without installing them in a random global place)
+* Aliasing using npm to replace react/react-dom with preact/compat is much nicer solution than the one on the preact wobsite. Thanks https://fettblog.eu/go-preact/
+* `npm run dev` to run dev server (in general, `npm run [script]` to run any command specified in the package.json's scripts section)
+    * Javascript will throw `JavaScript heap out of memory` error if you don't tell Node to have bigger heap (wtf?), so make sure to set `export NODE_OPTIONS='--max-old-space-size=8192'`. I did this in the 'package.json' so I don't have to remember.
+    * Again... wat?? why??
+* There's a difference between
+  ```
+  import DarkTheme from './plotly-themes.js'
+  ```
+  and
+  ```
+  import { DarkTheme } from './plotly-themes.js'
+  ```
+    * The former is trying to import a "default export" while the latter is trying to import a "named export". Not sure exactly what the distinction is
--- src/index.js
+++ src/index.js
@@ -1,11 +1,60 @@
 import './style';
 import { Component } from 'preact';
+import Plot from 'react-plotly.js';
+import { DarkTheme } from './plotly-themes.js';
+
+
+class Plop extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{
+				type: 'scatter',
+				mode: 'lines+points',
+				x: [1, 2, 3],
+				y: [2, 6, 3],
+				marker: {color: 'red'}
+				},
+				{
+				type: 'bar',
+				x: [1, 2, 3],
+				y: [2, 5, 3]
+				}
+			],
+			layout: {
+				template: DarkTheme,
+				title: 'A Fancy Plot',
+				autosize: true
+			},
+  			style: {width: "100%", height: "100%"},
+			frames: [],
+			config: {}
+		};
+	}
+
+	render() {
+		return (
+			<Plot
+				data={this.state.data}
+				useResizeHandler={true}
+				layout={this.state.layout}
+				style={this.state.style}
+				frames={this.state.frames}
+				config={this.state.config}
+				onInitialized={(figure) => this.setState(figure)}
+				onUpdate={(figure) => this.setState(figure)}
+			/>
+		)
+	}
+}
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Hello, World!</h1>
+				<Plop />
 			</div>
 		);
 	}
